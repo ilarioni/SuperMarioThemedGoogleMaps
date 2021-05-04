b623a70499b69d6dd4a1299e0efcfafdb87ea124
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-04 15:56:57 +0200

    map position

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function initMap(){
       });
 
     new google.maps.Marker({
-    position: { lat: -25.363, lng: 131.044 },
+    position: { lat: 34.66792617946361, lng: 135.43031308507628 },
     map,
     title: "Hello World!",
     });
 }
 
 // 34.668012480048375, 135.43014409327287
+
+// 34.66792617946361, 135.43031308507628
