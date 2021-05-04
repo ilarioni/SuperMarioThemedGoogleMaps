c68325cc3e4dacca3a540e694d9e914738ad49da
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-04 15:51:15 +0200

    adding map details

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8,
+        center: {lat: 34.668012480048375, lng: 135.43014409327287},
+        zoom: 18,
         mapId: '743b55a6eae40492'
       });
+
+    new google.maps.Marker({
+    position: { lat: -25.363, lng: 131.044 },
+    map,
+    title: "Hello World!",
+    });
 }
+
+// 34.668012480048375, 135.43014409327287
