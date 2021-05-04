9d397cc996bde6044c3cf51947a27455dfce58b1
ilarioni <ilia.meshvildishvili@gmail.com>
2021-05-04 15:44:19 +0200

    loading google maps

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8,
+        mapId: '743b55a6eae40492'
+      });
+}
